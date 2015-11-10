95b0f06acbd095694ddcfb4743bf79be0273ed11
npezza93 <npezza93@gmail.com>
2015-11-09 23:38:19 -0500

    Add subscription functionality

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,69 @@
+class SubscriptionsController < ApplicationController
+  before_action :set_subscription, only: [:show, :edit, :update, :destroy]
+
+  # GET /subscriptions
+  # GET /subscriptions.json
+  def index
+    @subscriptions = Stripe::Plan.all(:limit => 3).data
+  end
+
+  # GET /subscriptions/1
+  # GET /subscriptions/1.json
+  def show
+  end
+
+  # GET /subscriptions/new
+  def new
+    @subscription = Subscription.new
+  end
+
+  # GET /subscriptions/1/edit
+  def edit
+  end
+
+  # POST /subscriptions
+  # POST /subscriptions.json
+  def create
+    @subscription = Subscription.new(subscription_params)
+
+    respond_to do |format|
+      if @subscription.save_plan(params[:amount], params[:interval], params[:name])
+        format.html { redirect_to @subscription, notice: 'Subscription was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /subscriptions/1
+  # PATCH/PUT /subscriptions/1.json
+  def update
+    respond_to do |format|
+      if @subscription.update(subscription_params)
+        format.html { redirect_to @subscription, notice: 'Subscription was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /subscriptions/1
+  # DELETE /subscriptions/1.json
+  def destroy
+    @subscription.destroy
+    respond_to do |format|
+      format.html { redirect_to subscriptions_url, notice: 'Subscription was successfully destroyed.' }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_subscription
+      @subscription = Subscription.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def subscription_params
+      params.require(:subscription).permit(:plan_id)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110021159) do
+ActiveRecord::Schema.define(version: 20151110024432) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "first_name"
@@ -99,6 +99,12 @@ ActiveRecord::Schema.define(version: 20151110021159) do
     t.datetime "updated_at", null: false
     t.string   "title"
     t.text     "content"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string   "plan_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
