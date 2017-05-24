745fdeb70ed35d20aa841eb42b809aae921b8673
npezza93 <npezza93@gmail.com>
2017-05-24 18:16:42 -0400

    Run migrations on adding user to carts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160124224148) do
+ActiveRecord::Schema.define(version: 20170524215534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20160124224148) do
   create_table "carts", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "credit_cards", id: :serial, force: :cascade do |t|
