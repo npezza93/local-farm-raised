b0d0ed1756ad31c068791ca79af5178198b11ca6
npezza93 <npezza93@gmail.com>
2017-05-25 21:34:47 -0400

    Add fab helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def fab(link)
+    button_classes = "mdc-fab material-icons new-fab app-fab--absolute"
+
+    link_to link, class: button_classes, "data-mdc-auto-init" => "MDCRipple" do
+      content_tag(:span, "add", class: "mdc-fab__icon")
+    end
+  end
 end
