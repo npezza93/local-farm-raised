7a97b1fe97a8f6cad7b3c54ebddcad3162faf365
npezza93 <npezza93@gmail.com>
2017-05-27 09:59:47 -0400

    Fix cops in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,9 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :store_location
-
-  def store_location
-    # store last url - this is needed for post-login redirect to whatever the user last visited.
-    return unless request.get?
-    if (request.path != "/users/sign_in" &&
-        request.path != "/users/sign_up" &&
-        request.path != "/users/password/new" &&
-        request.path != "/users/password/edit" &&
-        request.path != "/users/confirmation" &&
-        request.path != "/users/sign_out" &&
-        !request.xhr?) # don't store ajax calls
-      session[:previous_url] = request.fullpath
-    end
-  end
 
   def current_cart
     @current_cart ||= Cart.find_or_create_by_session(session)
@@ -27,10 +14,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_cart
 
   private
-    def auth_user
-      if !current_user.try(:admin?)
-        redirect_to store_url, notice: "You are not authorized to view this page"
-      end
-    end
 
+  def auth_user
+    return unless current_user&.admin?
+
+    redirect_to store_url, notice: "You are not authorized to view this page"
+  end
 end
