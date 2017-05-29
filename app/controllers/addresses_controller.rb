0f9e1820a46d814183d39e0bc73c96b933509f43
npezza93 <npezza93@gmail.com>
2017-05-28 23:16:27 -0400

    Pass the referrer when creating a new address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -18,7 +18,8 @@ class AddressesController < ApplicationController
     @address = @addresses.new(address_params)
 
     if @address.save
-      redirect_to addresses_path, notice: "Successfully added to address book"
+      redirect_to post_create_redirect_path,
+                  notice: "Successfully added to address book"
     else
       render :new
     end
@@ -53,5 +54,11 @@ class AddressesController < ApplicationController
       :first_name, :last_name, :street_address1, :street_address2, :city,
       :state, :zipcode, :phone_number
     )
+  end
+
+  def post_create_redirect_path
+    return addresses_path unless params[:referrer] == new_order_url
+
+    new_order_path
   end
 end
