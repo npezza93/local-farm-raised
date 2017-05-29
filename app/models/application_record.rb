b131960ecefe8b7dfa50c6efbc0e03293b35c0ee
npezza93 <npezza93@gmail.com>
2017-05-29 19:47:26 -0400

    Move #save_to_stripe to application record and make more generic

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,14 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def save_to_stripe
+    self.class.transaction do
+      yield
+    end
+  rescue Stripe::InvalidRequestError => e
+    logger.error "Stripe error: #{e.message}"
+    errors.add :base, "Stripe error while saving: #{e.message}"
+    false
+  end
 end
