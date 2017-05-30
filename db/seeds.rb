75425302691c652229d9b9b91a7281aaccba0c70
npezza93 <npezza93@gmail.com>
2017-05-29 20:10:26 -0400

    Output progress when seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,15 @@
     description: Faker::Lorem.paragraphs(6).join,
     remote_image_url: Faker::LoremPixel.image("1920x1080", false, "food")
   )
+  puts "Created product ##{i}"
 end
 
-50.times do
+50.times do |i|
   User.create(
     name: Faker::StarWars.character,
     email: Faker::Internet.email,
     password: (password = SecureRandom.hex),
     password_confirmation: password
   )
+  puts "Created user ##{i}"
 end
